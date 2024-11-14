3215ed44ef978e1e14b1c783c66844f411cf3183
harshit-goyal524 <harshitgoyal524@gmail.com>
2024-11-14 19:28:41 +0530

    First Commit

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+const Dashboard = () => {
+  return (
+    <div>dashbrd</div>
+  )
+}
+export default Dashboard
---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import Dashboard from "@/app/dashboard/page";
+import DashboardWrapper from "./dashboardWrapper";
+
+export default function Home() {
+  return <Dashboard/>;
+}
